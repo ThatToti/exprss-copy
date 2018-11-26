1ccd771f4967cb0471926529a60cf172db60305d
ThatToti <mr.totihuang@gmail.com>
2018-11-26 22:38:30 +0800

    [+]方法;静态文件;路由 controller

---
+++ test/web.js
@@ -0,0 +1,56 @@
+
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+var path = require('path')
+
+/** control 模块 */
+var handles = {
+    index: {
+        index: function (req, res) {
+            var pathname = url.parse(req.url).pathname;
+            /** 读取静态文件 */
+            fs.readFile(path.resolve(__dirname, `../doc/${pathname}.html`), (err, file) => {
+                if (err) {
+                    res.writeHead(404, {
+                        'Content-Type': 'text/plain'
+                    })
+                    res.end('找不到文件')
+                    return
+                }
+
+                res.writeHead(200, {
+                    'Content-Type': 'text/plain',
+                })
+                res.end(file)
+            })
+        }
+    }
+}
+
+var server = http.createServer((req, res) => {
+
+
+    console.log(`请求的方法是:${req.method}`)
+
+    var pathname = url.parse(req.url).pathname;
+
+    var paths = pathname.split('/')
+
+    console.log(`请求的路径是:${pathname}`)
+
+    var controller = paths[1] || 'index'
+
+    var action = paths[2] || 'index'
+
+    var args = paths.slice(3)
+
+    /** 路由控制器 */
+    if (handles[controller] && handles[controller][action]) {
+        handles[controller][action].apply(null, [req, res].concat(args))
+    }
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('port', 8000)
+})
