14399da3f9a82ce333b45b281c939b0539366ec8
ThatToti <mr.totihuang@gmail.com>
2018-11-29 16:26:23 +0800

    [+]file download;json;302

---
+++ test/view.js
@@ -0,0 +1,49 @@
+var http = require('http')
+var fs = require('fs')
+var path = require('path')
+
+
+
+var server = http.createServer((req, res) => {
+
+    let filepath = path.resolve(__dirname, '../doc/index.css')
+
+    /** 下载附件方法 */
+    res.sendFile = (filepath) => {
+        fs.stat(filepath, (err, stat) => {
+
+            if (err) res.end(err)
+
+            var stream = fs.createReadStream(filepath)
+
+            res.writeHead(200, {
+                'Content-Type': 'text/css',
+                'Content-Length': stat.size,
+                'Content-Disposition': `attachment;filename='hello.css'`
+            })
+
+            stream.pipe(res)
+        })
+    }
+
+    /** respnese json */
+    res.json = (json) => {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        res.end(JSON.stringify(json))
+    }
+
+    /** 302 redirect */
+    res.redirect = (url) => {
+        res.writeHead(302, {
+            'Location': url
+        })
+        res.end('redirect to new address')
+    }
+
+    res.writeHead(200)
+    res.end(`<html><body>hello world</body></html>`)
+})
+
+server.listen(8000)
