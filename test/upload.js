547d1ad09d4d43fb47cd434ff703adba7e66c854
ThatToti <mr.totihuang@gmail.com>
2018-11-29 13:39:03 +0800

    [+]form,json,xml 上传处理模块

--- test/upload.js
+++ test/upload.js
@@ -2,20 +2,57 @@
 var http = require('http')
 var fs = require('fs')
 var crypto = require('crypto')
+var querystring = require('querystring')
 
+/** 生成随机数 */
 var generateRandom = function (len) {
-    return crypto.randomBytes(Math.ceil(len * 3 / 4))
-        .toString('base64')
-        .slice(0, len)
+	return crypto.randomBytes(Math.ceil(len * 3 / 4))
+		.toString('base64')
+		.slice(0, len)
+}
+
+/** 判断是否有 body 字段  */
+var hasBody = function (req) {
+	return 'transfer-encoding' in req.headers || 'content-length' in req.headers
+}
+
+var handle = function (req, res) {
+
+	let type = req.headers['content-type'].split(';')[0]
+
+	/** form */
+	if (type === 'application/x-www-form-urlencoded') {
+		req.body = querystring.parse(req.rawBody)
+
+		/** json */
+	} else if (type === 'application/json') {
+		req.body = Json.parse(req.rawBody)
+
+		/** xml */
+	} else if (type === 'application/xml') {
+		//xml 中间件去解析
+	}
+
+	res.writeHead(200, "OK")
+	res.end('form submit')
 }
 
 var server = http.createServer((req, res) => {
 
-    debugger
+	if (hasBody(req)) {
+		var buffers = []
 
-    var token = generateRandom(24)
-    res.writeHead(200)
-    res.end(`<input type="hidden" name='_csrf' value="${token}" />`)
+		req.on('data', (chunk) => {
+			buffers.push(chunk)
+		})
+
+		req.on('end', () => {
+			req.rawBody = Buffer.concat(buffers).toString()
+			handle(req, res)
+		})
+	} else {
+		handle(req, res)
+	}
 })
 
 server.listen(8000)
